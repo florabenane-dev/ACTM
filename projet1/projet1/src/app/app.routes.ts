d592c34910302408e9dd557b7671c1f49ecfde1f
Flora BENANE <flora.benane.pro@gmail.com>
2025-10-17 10:16:37 +0200

    refactor(view): add playlist page, delete home page and modify route

--- projet1/projet1/src/app/app.routes.ts
+++ projet1/projet1/src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
+  // {
+  //   path: 'home',
+  //   loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  // },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'playlist',
     pathMatch: 'full',
+  },
+  {
+    path: 'playlist',
+    loadComponent: () => import('./view/playlist/playlist.page').then( m => m.PlaylistPage)
   },
 ];
