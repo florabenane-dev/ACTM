eddb7f1492129d0071575435f87c98a7e5ae8264
Flora BENANE <flora.benane.pro@gmail.com>
2025-10-20 12:29:11 +0200

    feat(playlist): add radoi bouton

--- projet1/projet1/src/app/view/playlist/playlist.page.ts
+++ projet1/projet1/src/app/view/playlist/playlist.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonRow } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonGrid, IonItem, IonRow, IonRadio, IonCol, IonRadioGroup } from '@ionic/angular/standalone';
 import { SongComponent } from '../song/song.component';
 import { SongRepository } from 'src/app/repository/song-repository';
 import { Song } from 'src/app/data/song';
@@ -11,7 +11,7 @@ import { Song } from 'src/app/data/song';
   templateUrl: './playlist.page.html',
   styleUrls: ['./playlist.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonGrid, IonRow, SongComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonGrid, IonRow, IonItem, IonLabel, IonRadioGroup, IonRadio, IonCol, SongComponent]
 })
 export class PlaylistPage {
 
