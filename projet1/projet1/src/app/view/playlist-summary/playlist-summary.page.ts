8a6eb59b42a65ad0e3fbd533748922f621d574db
Flora Benane <flora.benane.pro@gmail.com>
2025-10-24 09:12:48 +0200

    feat(share): implement the button 'Send to app music' in mobile and in desktop

--- projet1/projet1/src/app/view/playlist-summary/playlist-summary.page.ts
+++ projet1/projet1/src/app/view/playlist-summary/playlist-summary.page.ts
@@ -5,6 +5,7 @@ import { IonBackButton, IonButton, IonContent, IonHeader, IonImg, IonItem, IonLa
 import { SongRepository } from 'src/app/repository/song-repository';
 import { Router } from '@angular/router';
 import { Song } from 'src/app/data/song';
+import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'app-playlist-summary',
@@ -42,7 +43,42 @@ export class PlaylistSummaryPage implements OnInit {
     console.log('[STATE] ', this.songs);
   }
 
-  onSharePlaylist() {
+  async onSharePlaylist() {
+    if(this.songs.length === 0) {
+      alert('Aucune music à partager')
+      return;
+    } 
+
+    const playlistText = this.songs
+    .map(song => song.title)
+    .join('\n');
+
+    const message = `\n\n${playlistText}`;
+    
+    try {
+      if (navigator.share) { //sur un navigateur, pour tester car pas android studio
+        await navigator.share({
+          title: 'Sharing Text',
+          text: message
+        });
+      }
+
+      else { //sur un tel
+        await Share.share({
+          title: 'Sharing Text',
+          text: message,
+          dialogTitle: 'Playlist'
+        });
+
+        //affichage desktop
+        await navigator.clipboard.writeText(message);
+        alert('text copie dans le presse-papier');
+      }
+
+    } catch (error) {
+      console.error('Erreur de partage :', error);
+      alert('Impossible de partager la playlist');
+    }
 
   }
 
