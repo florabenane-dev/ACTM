060674db478642b0491410a85ffb975af260c907
Flora Benane <flora.benane.pro@gmail.com>
2025-11-17 11:45:56 +0100

    chore(architecture): Architecture du projet et initialisation de la home page 'RecipesWidget'

---
+++ projet2/lib/ui/Home/widgets/RecipesWidget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RecipesWidget extends StatelessWidget {
+  const RecipesWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
