f096432d15c7aae5af5d1c0378bffed6eabd7a3d
Flora Benane <flora.benane.pro@gmail.com>
2025-11-18 13:40:32 +0100

    chore(images): Ajouter les photos de profil des recettes)

---
+++ projet2/lib/data/repositories/RecipeRepositoryDummyImpl.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:projet2/data/model/recipe.dart';
+import 'package:projet2/data/repositories/RecipesRepository.dart';
+
+class RecipeRepositoryDummyImpl implements RecipeRepository{
+  @override
+  List<Recipe> recipes = [
+
+  ];
+  
+  
+}
