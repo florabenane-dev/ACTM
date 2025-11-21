cbfa8cc69cc77717726fecfeeb625c14ce2b30e4
Flora Benane <flora.benane.pro@gmail.com>
2025-11-21 21:24:09 +0100

    feat(routing and view): Naviguer vers 'EditDeatils' and Ajout d'un champ text pour le 'prepTime'

--- projet2/lib/ui/recipeEditDetails.dart
+++ projet2/lib/ui/recipeEditDetails.dart
@@ -1,3 +1,53 @@
-class RecipeEditDetails {
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../model/data/recipe.dart';
+import '../model/repositories/recipesListPresenter.dart';
+
+class RecipeEditDetails extends StatelessWidget{
+
+  ///constructeur
+  const RecipeEditDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    /// passer les arguments de route
+    final recipe = ModalRoute.of(context)!.settings.arguments as Recipe;
+
+    /// s'abonner au ChangeNotifier
+    final recipesPresenter = context.watch<RecipesListPresenter>();
+
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 80,
+        title: Text(
+          recipe.name,
+          style: TextStyle(fontSize: 27),
+        ),
+      ),
+
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TextFormField(
+                initialValue: "${recipe.prepTime}",
+                onChanged: (text) {
+                  try {
+                    recipesPresenter.setPrepTime(recipe, int.parse(text));
+                  } on FormatException {
+                    //exception
+                  }
+                },
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: []
+          ),
+        ]),
+    ),
+    );
+  }
 
 }
