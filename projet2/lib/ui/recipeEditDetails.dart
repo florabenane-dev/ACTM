8e0353ad20ea1517ee4f25842d3d5c81914cb98d
Flora Benane <flora.benane.pro@gmail.com>
2025-11-22 14:31:14 +0100

    feat(ui: RecipeEditDetails): Ajout des champs de text pour les proprietés cookTime et Serving And Ajout de la Checkbox de SortBYName

--- projet2/lib/ui/recipeEditDetails.dart
+++ projet2/lib/ui/recipeEditDetails.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 import '../model/data/recipe.dart';
@@ -23,6 +24,8 @@ class RecipeEditDetails extends StatelessWidget{
     /// s'abonner au ChangeNotifier
     final recipesPresenter = context.watch<RecipesListPresenter>();
 
+    bool sortedByName = false;
+
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 80,
@@ -35,23 +38,30 @@ class RecipeEditDetails extends StatelessWidget{
         body: SingleChildScrollView (
           padding: const EdgeInsets.all(18),
           child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              TextFormField(
-                initialValue: "${recipe.prepTime}",
-                onChanged: (text) {
-                  try {
-                    recipesPresenter.setPrepTime(recipe, int.parse(text));
-                  } on FormatException {
-                    //exception
-                    // TODO : Ajouter la gestion d'exception
-                  }
-                },
+
+              // preparation time
+              SizedBox(
+                height: 50.0,
+                child: TextFormField(
+                  initialValue: "${recipe.prepTime}",
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  onChanged: (text) {
+                    try {
+                      recipesPresenter.setPrepTime(recipe, int.parse(text));
+                    } on FormatException {
+                      //exception
+                      // TODO : Ajouter la gestion d'exception
+                    }
+                  },
                   decoration: const InputDecoration(
                     label: Text("Prep Time (mins)"),
+                    labelStyle: TextStyle(fontSize: 14),
                     border: OutlineInputBorder(),
                     hintText: "Enter a preparation Time",
+                    hintStyle: TextStyle(fontSize: 15),
                     filled: true,
                     focusedBorder: OutlineInputBorder(
                       borderSide: BorderSide(
@@ -60,7 +70,93 @@ class RecipeEditDetails extends StatelessWidget{
                       ),
                     ),
                   ),
+                ),
               ),
+              const SizedBox(height: 20),
+
+              // cook time
+              SizedBox(
+                height: 50.0,
+                child: TextFormField(
+                  initialValue: "${recipe.cookTime}",
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  onChanged: (text) {
+                    try {
+                      recipesPresenter.setCookTime(recipe, int.parse(text));
+                    } on FormatException {
+                      //exception
+                      // TODO : Ajouter la gestion d'exception
+                    }
+                  },
+                  decoration: const InputDecoration(
+                    label: Text("Cook Time (mins)"),
+                    labelStyle: TextStyle(fontSize: 14),
+                    border: OutlineInputBorder(),
+                    hintText: "Enter a cook Time",
+                    hintStyle: TextStyle(fontSize: 15),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.indigo,
+                        width: 2.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              // serving
+              SizedBox(
+                height: 50.0,
+                child: TextFormField(
+                  initialValue: "${recipe.serving}",
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  onChanged: (text) {
+                    try {
+                      recipesPresenter.setServing(recipe, int.parse(text));
+                    } on FormatException {
+                      //exception
+                      // TODO : Ajouter la gestion d'exception
+                    }
+                  },
+                  decoration: const InputDecoration(
+                    label: Text("Serving"),
+                    labelStyle: TextStyle(fontSize: 14),
+                    border: OutlineInputBorder(),
+                    hintText: "Enter the number of serving",
+                    hintStyle: TextStyle(fontSize: 15),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.indigo,
+                        width: 2.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              // ingredients
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Ingredients",
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                  const SizedBox(width: 16),
+                  Checkbox(value: sortedByName,
+                      onChanged: (bool? value) {
+
+                      }
+                  )
+                ],
+              )
+
 
           //     Column(
           //       crossAxisAlignment: CrossAxisAlignment.start,
