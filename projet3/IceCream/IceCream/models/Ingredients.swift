77ffa95f47da6bd674dd089fec86bc9ce7998472
Flora BENANE <flora.benane.pro@gmail.com>
2025-12-10 13:35:44 +0100

    feat(data): changement de la strucrure des données pour simplifier l'affichage de la liste mixte

--- projet3/IceCream/IceCream/models/Ingredients.swift
+++ projet3/IceCream/IceCream/models/Ingredients.swift
@@ -5,16 +5,51 @@
 //  Created by benanflo on 08/12/2025.
 //
 
-/**
-    Modèle de données pour les ingredients extras de la glace
-        @name, @quantity, @unit, @price
- */
+
 
 import Foundation
 
+/// Définit la catégorie d'un ingrédient
+///     Utiliser pour classer les articles dans l'interface ou appliquer des logiques de prix différentes
+enum IngredientType {
+    /// parfum principal (Vanille, Chocolat, pistache)
+    case flavor
+    
+    /// support  (Coupe, cône)
+    case container
+    
+    /// garniture (éclats de noisettes, Chantilly)
+    case topping
+}
+
+/// Représente un ingrédient  dans l'inventaire.
+///     * contient les données de l'ingrédient (nom, prix) et son état dans l'interface de stock
 struct Ingredients {
-    var name: String
-    var quantity: Int
-    var unit: String
-    var price: Double
+    
+    // MARK: - Propriétés de l'inventaire
+    
+    // let id = UUID()
+    
+    /// Le nom
+    let name: String
+    
+    /// L'image des parfum, et qui nil pour les coupes et toping
+    let image: String?
+    
+    /// La quantité actuellement disponible en stock
+    let stockQuantity: Double
+    
+    /// L'unité de mesure associée à la quantité
+    let unit: String
+    
+    /// La catégorie (Parfum, Contenant ou Garniture)
+    let type: IngredientType
+    
+    /// Le prix unitaire
+    let price: Double
+    
+    // MARK: - État de l'interface
+    /// Indique si l'ingrédient est sélectionné dans la page "Check all items"
+    ///     * La valeur par défaut est `false`
+    var isSelectedForOrder: Bool = false
 }
