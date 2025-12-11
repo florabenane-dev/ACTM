b284b7571790ad2621c6ce119ecf57ac840063de
Flora BENANE <flora.benane.pro@gmail.com>
2025-12-11 16:14:51 +0100

    feat(mail) and fix(injector): Ajouter le service MainService pour envoyer un mail and Corriger l'injection dans Injector

--- projet3/IceCream/IceCream/Injector/Injector.swift
+++ projet3/IceCream/IceCream/Injector/Injector.swift
@@ -11,5 +11,5 @@ import Foundation
 ///     * Pour avoir une seule source de vérité accessible partout => Pattern Singleton
 class Injector {
     /// Unique et accessible dans toute l'app avec Injector.iceCreamRepository
-    static let iceCreamRepository : IceCreamRepository = IceCreamRepositoryDummy()
+    static let iceCreamRepository = IceCreamRepositoryDummy()
 }
--- projet3/IceCream/IceCream/services/MailService.swift
+++ projet3/IceCream/IceCream/services/MailService.swift
@@ -11,3 +11,38 @@
  */
 
 import Foundation
+import UIKit
+
+class MailService {
+    
+    /// Ouvre l'application Mail pré-remplie
+       static func sendOrder(items: [String], signature: String) {
+           // 1. Préparation du contenu
+           let recipient = "order@icecream.com"
+           let subject = "Order"
+          
+           let itemsList = items.map { "* \($0)" }.joined(separator: "\n")
+           let body = """
+           Hi,
+           Please order the following:
+           \(itemsList)
+           
+           \(signature)
+           """
+          
+           // 2. Création de l'URL mailto
+           // Attention : Les URL ne supportent pas les espaces ou les retours à la ligne bruts.
+           // Il faut les encoder (ex: espace devient %20)
+           if let encodedSubject = subject.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+              let encodedBody = body.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+              let url = URL(string: "mailto:\(recipient)?subject=\(encodedSubject)&body=\(encodedBody)") {
+              
+               // 3. Ouverture de l'URL par le système iOS
+               if UIApplication.shared.canOpenURL(url) {
+                   UIApplication.shared.open(url)
+               } else {
+                   print("Impossible d'ouvrir l'application Mail (Simulateur ou pas de compte configuré)")
+               }
+           }
+       }
+}
