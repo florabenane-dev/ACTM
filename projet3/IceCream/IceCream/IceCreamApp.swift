81a6f0f914c4a699b780eaaa2dd5c76e3097f843
Flora BENANE <flora.benane.pro@gmail.com>
2025-12-11 16:56:26 +0100

    feat(navigation): Ajouter la navigation dans IceCreamApp

--- projet3/IceCream/IceCream/IceCreamApp.swift
+++ projet3/IceCream/IceCream/IceCreamApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct IceCreamApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                MainView()
+            }
         }
     }
 }
--- projet3/IceCream/IceCream/views/MainView.swift
+++ projet3/IceCream/IceCream/views/MainView.swift
@@ -14,6 +14,25 @@
 import SwiftUI
 
 struct MainView: View {
+    // Injection du Repository via le Singleton Injector
+        @StateObject var iceCreamRepository = Injector.iceCreamRepository
+        
+        // États locaux
+        @State private var selectedFlavors: [String: Int] = ["Chocolate": 0, "Vanilla": 0, "Pistachio": 0]
+        @State private var selectedContainer: String = "Cup"
+        @State private var selectedToppings: [String: Bool] = ["Whipped cream": false, "Hazelnuts": false]
+        
+        // Propriétés calculées
+        var totalScoops: Int { selectedFlavors.values.reduce(0, +) }
+        
+        var totalPrice: Double {
+            let containerPrice = iceCreamRepository.getIngredient(named: selectedContainer)?.price ?? 0.0
+            let toppingsPrices = selectedToppings.compactMap { (name, isSelected) -> Double? in
+                return isSelected ? iceCreamRepository.getIngredient(named: name)?.price : nil
+            }
+            return PricingService.calculateTotalPrice(scoopCount: totalScoops, containerPrice: containerPrice, extrasPrices: toppingsPrices)
+        }
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
