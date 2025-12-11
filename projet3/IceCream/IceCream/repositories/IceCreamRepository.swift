5a5c1693a26508cf21530d7e657f6e7252821276
Flora BENANE <flora.benane.pro@gmail.com>
2025-12-11 15:32:43 +0100

    feat(repository): créer le protocole abstrait pour le repository et l'implèmentation and Ajouter les données ficitfs

--- projet3/IceCream/IceCream/repositories/IceCreamRepository.swift
+++ projet3/IceCream/IceCream/repositories/IceCreamRepository.swift
@@ -7,4 +7,15 @@
 
 import Foundation
 
-/// 
+/// Contient les spécifications d'API, expose une liste abstraite d'ingredients en lecture
+
+protocol IceCreamRepository {
+    /// Liste abstraite accessible en lecture et écriture
+    var ingredients: [Ingredient] { get set }
+    
+    /// Fonction pour baisser le stock
+    func decreaseStock(ingredientName: String, amount: Double)
+    
+    /// Fonction pour récuperer un ingredient par son nom
+    func getIngredient(named name: String) -> Ingredient?
+}
--- projet3/IceCream/IceCream/repositories/IceCreamRepositoryDummy.swift
+++ projet3/IceCream/IceCream/repositories/IceCreamRepositoryDummy.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+/// Implementation concrète du repository IceCreamRepository
+class IceCreamRepositoryDummy: IceCreamRepository {
+    
+    var ingredients: [Ingredient] = [
+        // Parfums
+        Ingredient(name: "Chocolate", image: "chocolate", stockQuantity: 300, unit: "ml", type: .flavor, price: 0),
+        Ingredient(name: "Vanilla", image: "vanilla", stockQuantity: 100, unit: "ml", type: .flavor, price: 0),
+        Ingredient(name: "Pistachio", image: "pistachio", stockQuantity: 0, unit: "ml", type: .flavor, price: 0),
+        
+        // Contenants
+        Ingredient(name: "Cone", image: nil, stockQuantity: 2, unit: "unit", type: .container, price: 1.0),
+        Ingredient(name: "Cup", image: nil, stockQuantity: 7, unit: "unit", type: .container, price: 0.0),
+                
+        // Supplèments
+        Ingredient(name: "Whipped cream", image: nil, stockQuantity: 150, unit: "ml", type: .topping, price: 0.95),
+        Ingredient(name: "Hazelnuts", image: nil, stockQuantity: 100, unit: "g", type: .topping, price: 1.30)
+    ]
+    
+    /// baisser le stock
+    func decreaseStock(ingredientName: String, amount: Double) {
+        <#code#>
+    }
+    
+    /// récuperer un ingredient par son nom
+    func getIngredient(named name: String) -> Ingredient? {
+        <#code#>
+    }
+}
