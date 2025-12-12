bf844c0b6b7a7cdca58a53ecb60b5ad79c285310
Flora BENANE <flora.benane.pro@gmail.com>
2025-12-12 17:56:45 +0100

    feat(alertviewmodel): Implementer le alertviewmodel qui va gerer la popup et l'appel au service mail

--- projet3/IceCream/IceCream/viewmodels/alertViewModel.swift
+++ projet3/IceCream/IceCream/viewmodels/alertViewModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+class AlertViewModel: ObservableObject {
+    // ÉTAT
+    @Published var signature: String = "Thanks, MB"
+    @Published var showStock: Bool = false
+    
+    // Gestion de la popup
+    @Published var showMailAlert: Bool = false
+    @Published var mailContent: String = ""
+    
+    
+    // LOGIQUE
+    /// Envoie l'alerte pour le parfum vide
+    func sendAlert(for flavorName: String) {
+        let itemToOrder = "\(flavorName) icecream"
+        
+        // Appel au service mail
+        mailContent = MailService.generateMailBody(items: [itemToOrder], signature: signature)
+        MailService.sendOrder(items: [itemToOrder], signature: signature)
+        
+        showMailAlert = true
+    }
+    
+    /// Bascule l'affichage du stock
+    func toggleStockVisibility() {
+        showStock.toggle()
+    }
+}
